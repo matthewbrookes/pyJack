7c6a6a8324b821a39241abca0ae87ae7235af421
Matt Brookes <matt@mattonair.me.uk>
2012-12-17 19:03:47 +0000

    Added create_deck function to blackjack.py
    Have created this function in order to create a playing deck of 52 cards
    for use in this blackjack game.

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,20 @@
+import random
 
+suits = ["suits", "clubs", "hearts", "diamonds"]
+ranks = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+cards = []
+
+def create_deck():
+        for i in range(4):
+                card = []
+                for j in range(13):
+                        suit = suits[i]
+                        rank = ranks[j]
+                        card.append(suit)
+                        card.append(rank)
+                        cards.append(card)
+                        card = []
+                        
+
+create_deck()
+print cards
