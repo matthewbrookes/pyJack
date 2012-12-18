924a1404b01342fc378f1e4311a809430c86e98c
Matt Brookes <matt@mattonair.me.uk>
2012-12-18 08:05:06 +0000

    Created functions deal_card and game_intro in blackjack.py
    The first function takes the deck as input and returns a random card as well as removing said card from deck.
    The other function creates the intro to be displayed at the beginning of the game.

--- blackjack.py
+++ blackjack.py
@@ -2,30 +2,47 @@ import random
 
 suits = ["suits", "clubs", "hearts", "diamonds"]
 ranks = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
-deck = []
+player_hand = []
+dealer_hand = []
 
-def create_deck():
-        for i in range(4):
+def new_deck(): # This function creates a deck of 52 cards
+        deck = []
+        for i in range(4): # Iterates over number of suits in classic deck
                 card = []
-                for j in range(13):
-                        suit = suits[i]
+                for j in range(13): # Iterates over number of ranks in classic deck
+                        suit = suits[i] # Assigns suit to the value from the array 
                         rank = ranks[j]
-                        card.append(suit)
+                        card.append(suit) # Appends suit to the temporary card array
                         card.append(rank)
-                        deck.append(card)
-                        card = []
+                        deck.append(card) # Appends the card to the deck array
+                        card = [] # Resets card array for next iteration
+        return deck
                         
-def shuffle_deck(deck):
+def shuffle_deck(deck): # This function shuffles inputed deck
         new_deck = []
         random_index = []
-        for i in range(52):
-                random_index.append(i)
-        for i in range(len(random_index)-1):
-                r = random_index[random.randint(0, len(random_index)-1)]
-                new_deck.append(deck[r])
-                random_index.remove(r)
+        for i in range(52): # For every card
+                random_index.append(i) # Add a number 0-51 to the array
+        for i in range(len(random_index)-1): # For every value of the random_index array
+                r = random_index[random.randint(0, len(random_index)-1)] # Selects a random value from random_index array
+                new_deck.append(deck[r]) # Adds a random card to the new_deck array
+                random_index.remove(r) # Removes the random number from random_index
         return new_deck        
                 
-create_deck()
+def deal_card(deck): # This function returns a random card from inputed deck and removes it from deck
+        rIndex = random.randint(0,len(deck)-1)
+        c = deck[rIndex]
+        deck.remove(deck[rIndex])
+        return c
+def game_intro(): # The function to be ran at the start to introduce the game
+        print "Welcome to pyJack, a game of blackjack made in Python"
+        print "You will play against an AI dealer programmed like a professional"
+        decision = str(raw_input("Shall we begin? (Yes/No)"))
+        
+deck = new_deck()
 deck = shuffle_deck(deck)
-print deck
+
+player_hand.append(deal_card(deck))
+player_hand.append(deal_card(deck))
+
+game_intro()
