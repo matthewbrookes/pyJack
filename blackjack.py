c029f7b012b2cc3b4ad744d5c75547f0ec6394f8
Matt Brookes <matt@mattonair.me.uk>
2012-12-18 17:56:33 +0000

    Created print_hand function
    This function prints out the inputed hand in a friendly format. The
    function will add another card regardless of points unlike in real
    blackjack and is therefore not complete

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+# This program is a simple game of Blackjack made in Python
+
 import random
 import sys
 import time
@@ -36,7 +38,8 @@ def deal_card(deck): # This function returns a random card from inputed deck and
         c = deck[rIndex]
         deck.remove(deck[rIndex])
         return c
-def game_intro(): # The function to be ran at the start to introduce the game
+        
+def game_intro(): # This function is ran at the start to introduce the game
         print "Welcome to pyJack, a game of blackjack made in Python"
         print "You will play against an AI dealer programmed like a professional"
         decision = str(raw_input("Shall we begin? (Yes/No)"))
@@ -44,16 +47,46 @@ def game_intro(): # The function to be ran at the start to introduce the game
                 print "Good, let us start"
         elif decision.upper() == "NO" or decision.upper() == "N":
                 print "Goodbye"
-                time.sleep(1)
+                time.sleep(0.5)
                 sys.exit()
         else:
-                print "I didn't understand"
-                game_intro()
-        
+                print "I'm sorry I didn't understand"
+                print ""
+                game_intro()       
+
+def print_hand(hand): # This function is ran to print the hand to the user and allow them to get a new card   
+        print "Your hand contains:"
+        print ""
+        for i in range(len(hand)):
+                suit = hand[i][0]
+                rank = hand[i][1]
+                if rank == 'K':
+                        rank = "King"
+                elif rank == 'Q':
+                        rank = "Queen"
+                elif rank == 'J':
+                        rank = "Jack"
+                elif rank == 'A':
+                        rank = "Ace"
+                print "%s of %s" % (rank, suit)
+        print ""
+        if len(hand) < 5: # If there are less than 5 cards allow the user to add another card
+                decision = str(raw_input("Would you like another card? (Yes/No)"))
+                if decision.upper() == "YES" or decision.upper() == "Y":
+                        player_hand.append(deal_card(deck))
+                        print_hand(hand)
+                elif decision.upper() == "NO" or decision.upper() == "N":
+                        return
+                else:
+                        print "I'm sorry I didn't understand"
+                        print ""
+                        time.sleep(0.5)
+                        print_hand(hand)
+                        
 deck = new_deck()
 deck = shuffle_deck(deck)
 
 player_hand.append(deal_card(deck))
 player_hand.append(deal_card(deck))
 
-game_intro()
+print_hand(player_hand)
